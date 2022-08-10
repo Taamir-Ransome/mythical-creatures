48544c7e471e9ffb1caf046a3fb87e2a68f7f823
Taamir Ransome <taamir.ransome@sofwarellc.com>
2022-08-10 17:29:24 -0400

    Create unicorn class

---
+++ unicorn/lib/unicorn.rb
@@ -0,0 +1,10 @@
+# Path: mythical-creatures/unicorn/lib/unicorn.rb
+
+# Practice: ruby and git with Stemmle
+class Unicorn
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
