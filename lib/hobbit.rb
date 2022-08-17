10000ea7ba3eba6377be1761b94ae0ad8b456144
Taamir Ransome <taamir.ransome@sofwarellc.com>
2022-08-17 11:01:06 -0400

    Add attr dispositoion to hobbit

--- lib/hobbit.rb
+++ lib/hobbit.rb
@@ -2,12 +2,12 @@
 # Path: mythical-creatures/lib/hobbit.rb
 
 # Practice: ruby and git with Stemmle
-
 class Hobbit
-  attr_reader :name
+  attr_reader :name, :disposition
 
-  def initialiaze(name)
+  def initialiaze(name, disposition = 'homebody')
     @name = name
+    @disposition = disposition
   end
 end
 
